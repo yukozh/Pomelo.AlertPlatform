09aa0d12c5557a4e2d7665b6519dc37cbd7aa503
あまみや ゆうこ <1@1234.sh>
2018-01-08 08:47:44 +0800

    Configure startup

--- src/Pomelo.AlertPlatform.API/Startup.cs
+++ src/Pomelo.AlertPlatform.API/Startup.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,30 +15,28 @@ namespace Pomelo.AlertPlatform.API
             services.AddConfiguration(out var Config);
             services.AddEntityFrameworkMySql()
                 .AddDbContext<AlertContext>(x => x.UseMySql(Config["Database"]));
-            services.AddIdentity<User, IdentityRole<Guid>>(x =>
-            {
-                x.Password.RequireDigit = false;
-                x.Password.RequiredLength = 0;
-                x.Password.RequireLowercase = false;
-                x.Password.RequireNonAlphanumeric = false;
-                x.Password.RequireUppercase = false;
-                x.User.AllowedUserNameCharacters = null;
-            })
-          .AddDefaultTokenProviders()
-          .AddEntityFrameworkStores<AlertContext>();
+            services
+                .AddIdentity<User, IdentityRole<Guid>>(x =>
+                {
+                    x.Password.RequireDigit = false;
+                    x.Password.RequiredLength = 0;
+                    x.Password.RequireLowercase = false;
+                    x.Password.RequireNonAlphanumeric = false;
+                    x.Password.RequireUppercase = false;
+                    x.User.AllowedUserNameCharacters = null;
+                })
+              .AddDefaultTokenProviders()
+              .AddEntityFrameworkStores<AlertContext>();
+            services.AddMvc();
+            services.AddSmartUser<User, Guid>();
         }
-        
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDeveloperExceptionPage();
+            app.UseAuthentication();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
